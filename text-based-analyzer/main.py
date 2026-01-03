769d6e4eae41aa7fa6c0162169265a0a4bd6344b
Apurv Saktepar <apurvsaktepar2806@gmail.com>
2026-01-03 23:30:45 +0530

    updated

--- text-based-analyzer/main.py
+++ text-based-analyzer/main.py
@@ -30,7 +30,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://mannkibaat-rehab.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
